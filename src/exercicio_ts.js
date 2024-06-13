8c3ea654982921d3221ea03d64fa8135d1b8e15c
Wellivelton Fonseca <welliveltonfonseca@gmail.com>
2024-06-13 10:02:57 -0300

    commit inicial

---
+++ src/exercicio_ts.js
@@ -0,0 +1,5 @@
+"use strict";
+var multiplicador = function (base, altura) { return base * altura; };
+function dizOla() {
+    return this.nome + " disse oi";
+}
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,6 @@
+const multiplicador = (base: number, altura: number): number => base * altura;
+
+
+function dizOla(): string {
+    return `${this.nome} disse oi`
+}
